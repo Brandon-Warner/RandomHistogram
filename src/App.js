88098f2b7622510fde16fcca647a14cc2dd70b3c
Brandon Warner <brandonswarner5@gmail.com>
2021-12-09 13:13:59 -0800

    chart will render successfully, but will go blank on refresh

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {
-    BarChart,
-    Bar,
-    Cell,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend,
-    ResponsiveContainer
-} from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 const App = () => {
     const [numbers, setNumbers] = useState([]);
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         fetchData();
+        countNumbers(numbers);
     }, [setNumbers]);
 
     const fetchData = async () => {
@@ -33,6 +25,7 @@ const App = () => {
     console.log('numbers: ', numbers);
 
     const countNumbers = numbers => {
+        let countZero = 0;
         let countOne = 0;
         let countTwo = 0;
         let countThree = 0;
@@ -42,7 +35,7 @@ const App = () => {
         let countSeven = 0;
         let countEight = 0;
         let countNine = 0;
-        let countTen = 0;
+
         for (let i = 0; i < numbers.length; i++) {
             // console.log('numbers[i]: ', numbers[i]);
             switch (numbers[i]) {
@@ -73,30 +66,68 @@ const App = () => {
                 case '9':
                     countNine++;
                     continue;
-                case '10':
-                    countTen++;
+                case '0':
+                    countZero++;
                     continue;
                 default:
             }
         }
-        return [
-            countOne,
-            countTwo,
-            countThree,
-            countFour,
-            countFive,
-            countSix,
-            countSeven,
-            countEight,
-            countNine,
-            countTen
-        ];
+        setResults([
+            {
+                name: '0',
+                data: countZero
+            },
+            {
+                name: '1',
+                data: countOne
+            },
+            {
+                name: '2',
+                data: countTwo
+            },
+            {
+                name: '3',
+                data: countThree
+            },
+            {
+                name: '4',
+                data: countFour
+            },
+            {
+                name: '5',
+                data: countFive
+            },
+            {
+                name: '6',
+                data: countSix
+            },
+            {
+                name: '7',
+                data: countSeven
+            },
+            {
+                name: '8',
+                data: countEight
+            },
+            {
+                name: '9',
+                data: countNine
+            }
+        ]);
     };
 
-    console.log('countNumber: ', countNumbers(numbers));
+    console.log('results: ', results);
     return (
         <div>
             <h1>Histogram</h1>
+            <ResponsiveContainer width='100%' aspect={3}>
+                <BarChart width={800} height={300} data={results}>
+                    <CartesianGrid strokeDasharray='3 3' />
+                    <XAxis dataKey='name' />
+                    <YAxis />
+                    <Bar dataKey='data' fill='lightblue'/>
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
